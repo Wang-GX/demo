7864416b9810cca7e05608efa0db57208a00704f
wangguangxi <gxwang@wisepool.com.cn>
2019-11-12 15:39:33 +0800

    优化代码和注释

--- src/main/java/com/example/demo/config/CustomizeConfig.java
+++ src/main/java/com/example/demo/config/CustomizeConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.PropertySource;
  * 需要在配置类中指定自定义配置文件的位置
  */
 @Configuration
-@PropertySource(value = {"classpath:my.properties","classpath:my2.properties"})//指定自定义配置文件的位置
+@PropertySource(value = {"classpath:my.properties","classpath:my2.properties"})//TODO 指定自定义配置文件的位置
 //@PropertySource 和 @Value 组合使用，可以将自定义属性文件中的属性变量值注入到当前类的使用@Value注解的成员变量中。
 //@PropertySource 和 @ConfigurationProperties 组合使用，可以将属性文件与一个Java类绑定，将属性文件中的变量值注入到该Java类的成员变量中。
 @ConfigurationProperties(prefix = "my")//指定前缀
--- src/test/java/com/example/demo/MyTest.java
+++ src/test/java/com/example/demo/MyTest.java
@@ -4,7 +4,6 @@ import com.example.demo.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
