ea08ec66990057c20444811c1f3b98392e059328
wangguangxi <gxwang@wisepool.com.cn>
2019-11-22 18:03:25 +0800

    测试数据库事务常用的两个传播行为 增加待完成项 删除错误使用的@Repository注解

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,9 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.pojo.User;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserMapper {
 
     User getUser();
