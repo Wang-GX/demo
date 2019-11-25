4cca20cfdc1adfad09180fbd9a1bca7bef0e38e2
wangguangxi <gxwang@wisepool.com.cn>
2019-11-25 20:09:44 +0800

    FastJson标准预期结果

---
+++ src/test/java/com/example/demo/FastJson/FastJsonTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.FastJson;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.example.demo.pojo.User;
+
+public class FastJsonTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUserName("张三");
+        user.setUserSex("1");
+        user.setUserAge("18");
+
+        //标准预期结果
+        String str = JSONObject.toJSONString(user, SerializerFeature.WriteMapNullValue);
+        System.out.println(str);
+
+    }
+}
