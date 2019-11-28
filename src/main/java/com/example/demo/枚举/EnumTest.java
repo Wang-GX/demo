f00293cf7a7df6c84da5c5267a32ab38981fd254
wangguangxi <gxwang@wisepool.com.cn>
2019-11-28 17:43:10 +0800

    枚举使用

---
+++ src/main/java/com/example/demo/枚举/EnumTest.java
@@ -0,0 +1,8 @@
+package com.example.demo.枚举;
+
+public class EnumTest {
+    public static void main(String[] args) {
+
+        System.out.println(MyEnum.CONSTANT1.getCode()+":"+MyEnum.CONSTANT1.getMessage());
+    }
+}
--- src/main/java/com/example/demo/枚举/MyEnum.java
+++ src/main/java/com/example/demo/枚举/MyEnum.java
@@ -1,4 +1,25 @@
 package com.example.demo.枚举;
 
-public class MyEnum {
+public enum  MyEnum {
+
+    CONSTANT1("1000","常量1"),
+    CONSTANT2("1000","常量2"),
+    CONSTANT3("1000","常量3");
+
+    MyEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    private String code;
+    private String message;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
