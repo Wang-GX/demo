6cdfca82b06be13b66301169e0b5084fde385f0a
wangguangxi <gxwang@wisepool.com.cn>
2019-12-03 17:21:13 +0800

    使用Redisson实现分布式锁，测试完毕。 调整分布式锁测试预期结果。

---
+++ src/main/java/com/example/demo/定时任务_分布式锁/Redission/RedissionConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.定时任务_分布式锁.Redission;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissionConfig {
+
+    @Bean
+    public RedissonClient getClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+}
+
