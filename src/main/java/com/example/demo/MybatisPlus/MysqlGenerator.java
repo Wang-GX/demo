bbb70a3ce73651b82a9db8a7141dc9f988fc58cf
asus <s523373295@163.com>
2019-12-15 21:53:42 +0800

    MybatisPlus代码生成器

--- src/main/java/com/example/demo/MybatisPlus/MysqlGenerator.java
+++ src/main/java/com/example/demo/MybatisPlus/MysqlGenerator.java
@@ -1,23 +1,16 @@
 package com.example.demo.MybatisPlus;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
-import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.TemplateConfig;
-import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -26,24 +19,11 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
  */
 public class MysqlGenerator {
 
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public static String scanner(String tip) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotEmpty(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
-    }
+    //数据库配置
+    private static final String DRIVERNAME = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/local_test?useUnicode=true&useSSL=false&characterEncoding=utf8";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     /**
      * RUN THIS
@@ -56,75 +36,49 @@ public class MysqlGenerator {
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("wgx");
+        //gc.setFileOverride(true);//是否覆盖文件
         gc.setOpen(false);
-        //gc.setSwagger2(true); 实体属性 Swagger2 注解
+        gc.setAuthor("wgx");
+        gc.setSwagger2(true);
+        gc.setActiveRecord(true);
+        //gc.setBaseResultMap(true);
+        //gc.setBaseColumnList(true);
+        gc.setServiceName("%sService");//自定义文件命名，注意 %s 会自动填充表实体属性！
         mpg.setGlobalConfig(gc);
 
         //数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/local_test?useUnicode=true&useSSL=false&characterEncoding=utf8");
-        //dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("root");
+        dsc.setDriverName(DRIVERNAME);
+        dsc.setUrl(URL);
+        dsc.setUsername(USERNAME);
+        dsc.setPassword(PASSWORD);
         mpg.setDataSource(dsc);
 
         //包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
-        pc.setParent("cn..baomidou.ant");
+        pc.setParent("com.wjj.application");
         mpg.setPackageInfo(pc);
-
-        //自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                //to do nothing
-            }
-        };
-
-        //如果模板引擎是 freemarker
-        String templatePath = "/templates/mapper.xml.ftl";
-        //如果模板引擎是 velocity
-        //String templatePath = "/templates/mapper.xml.vm";
-
-        //自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
-        //自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                //自定义输入文件名称，如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-
-        // 配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
-        templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
 
         //策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
-        //公共父类
-        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
-        //写于父类中的公共字段
-        strategy.setSuperEntityColumns("id");
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix(pc.getModuleName() + "_");
+        //添加乐观锁版本号注解
+        strategy.setVersionFieldName("version");
+        //添加逻辑删除注解
+        strategy.setLogicDeleteFieldName("deleted");
+        //添加自动填充属性
+        List<TableFill> tableFillList = new ArrayList<>();
+        tableFillList.add(new TableFill("create_time", FieldFill.INSERT));
+        tableFillList.add(new TableFill("create_user", FieldFill.INSERT));
+        tableFillList.add(new TableFill("deleted", FieldFill.INSERT));
+        tableFillList.add(new TableFill("update_time", FieldFill.UPDATE));
+        tableFillList.add(new TableFill("update_user", FieldFill.UPDATE));
+        strategy.setTableFillList(tableFillList);
         mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        //mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
     }
 }
